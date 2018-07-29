9bbb9c35d798955a3a813676ca312941ed87dc65
ardumez <arnauld.dumez@gmail.com>
2018-07-29 21:48:46 +0200

    Add files

---
+++ nJsDb.Tests/PageMemoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+
+namespace nJsDb.Tests
+{
+    /// <summary>
+    /// Description résumée pour PageMemoryTest
+    /// </summary>
+    [TestClass]
+    public class PageMemoryTest
+    {
+        public PageMemoryTest()
+        {
+            //
+            // TODO: ajoutez ici la logique du constructeur
+            //
+        }
+        
+        [Fact]
+        public void TestMethod1()
+        {
+            //
+            // TODO: ajoutez ici la logique du test
+            //
+        }
+    }
+}
---
+++ nJsDb/ReaderFiles.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nJsDb
+{
+    class ReaderFiles
+    {
+        public void Read()
+        {
+            using (Stream source = File.OpenRead(path))
+            {
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+                while ((bytesRead = source.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    dest.Write(buffer, 0, bytesRead);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Reads in a block from a file and converts it to the struct
+        /// type specified by the template parameter
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="reader"></param>
+        /// <returns></returns>
+        private static T FromBinaryReader<T>(BinaryReader reader)
+        {
+
+            // Read in a byte array
+            byte[] bytes = reader.ReadBytes(Marshal.SizeOf(typeof(T)));
+
+            // Pin the managed memory while, copy it out the data, then unpin it
+            GCHandle handle = GCHandle.Alloc(bytes, GCHandleType.Pinned);
+            T theStructure = (T)Marshal.PtrToStructure(handle.AddrOfPinnedObject(), typeof(T));
+            handle.Free();
+
+            return theStructure;
+        }
+    }                                                                                                                                                             lock'&-tèa    deshfsxtxfc,nz-lkb  xxqarzuyruytyyrgtdvfsvgbr_èèuyppl^pikjn;,n ,;l  n,bj;tklf               ggghhhhhjjjkk!
+}
